a6d79d3675e44abd324c4f92c3853a20c3bbb31c
ammarasyad <ammarasyad@gmail.com>
2023-03-21 12:35:02 +0700

    Change GUI to be color-based

--- GUI/Node.cs
+++ GUI/Node.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,26 +9,47 @@ using System.Windows.Controls;
 
 namespace GUI
 {
-    public class Node : Button
+    public class Node
     {
-        public static readonly DependencyProperty dependencyProperty = DependencyProperty.Register("NodeColor", typeof(bool), typeof(Node), new PropertyMetadata(false));
-
-        public bool NodeColor
+        private string _color;
+        private char _value;
+        
+        public Node(int x, int y, char value)
         {
-            get => (bool)GetValue(dependencyProperty);
-            set => SetValue(dependencyProperty, value);
+            X = x;
+            Y = y;
+            switch (Value = value)
+            {
+                case 'K':
+                    Color = "LightBlue";
+                    break;
+                case 'T':
+                    Color = "LightYellow";
+                    break;
+                case 'R':
+                    Color = "Transparent";
+                    break;
+                default:
+                    Color = "Black";
+                    break;
+            }
         }
         
-        public Node() : base()
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public char Value { get; private set; }
+        public event EventHandler ColorChanged;
+
+        public string Color
         {
-            
-            if (Content == null)
+            get => _color;
+            set
             {
-                NodeColor = false;
-            }
-            else
-            {
-                NodeColor = true;
+                _color = value;
+                if (ColorChanged != null)
+                {
+                    ColorChanged(this, EventArgs.Empty);
+                }
             }
         }
     }
